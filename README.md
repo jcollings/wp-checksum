89ec0bd8f5a3177835f643ffa9f02642232df1da
James Collings <james@jclabs.co.uk>
2013-03-11 16:43:21 +0000

    Initial Development

---
+++ README.md
@@ -0,0 +1,12 @@
+Md5 Checksum Generator
+================
+Md5 Checksum Generator is a wordpress plugin that allows you to add links to related media, you can customize what can be linked to from the plugins settings menu.
+
+Installation
+------------
+1. Extract the folder to "jc-md5-hasher" to your wordpress installs plugin directory.
+2. Login to wordpress administration area and activate the plugin.
+
+Change Log
+----------
+Ver 0.0.1 -  Initial Development
---
+++ md5-hash.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: MD5 Hasher
+Plugin URI: http://www.jclabs.co.uk
+Description: Security Process to show latest modified/created files, run as frequently as deemed fit, an email is sent automatically with the results.	
+Author: James Collings
+Version: 0.0.1
+Author URI: http://www.jclabs.co.uk
+*/
+
+define('MD5_HASHER_DIR', plugin_dir_path( __FILE__ ));
+
+class Md5_Hasher{
+
+	private $dir = ABSPATH;
+	private $file_check = 'md5_checksums.txt';
+	private $file_change = 'md5_changes.txt';
+	private $compare = true;
+	
+	public function __construct(){
+		add_action('Md5_Checksum', array($this, 'run_hash_check'));
+		add_action('wp', array($this, '_activate'));
+	}
+
+	public function _activate() {
+	    if ( !wp_next_scheduled( 'Md5_Checksum' ) ) {
+	        wp_schedule_event( time(), 'hourly', 'Md5_Checksum');
+	    }
+	}
+
+	public function run_hash_check() {
+
+		$md5_gen_old = array();
+		$md5_gen_output = array();
+        $md5_changed_output = array();
+
+		if(!is_file(MD5_HASHER_DIR . $this->file_check)){
+			$this->compare = false;
+			$fh = fopen(MD5_HASHER_DIR . $this->file_check, 'x');
+			
+		}else{
+			$fh = fopen(MD5_HASHER_DIR . $this->file_check, 'r');
+			$md5_gen_old = (Array)json_decode(fread($fh, filesize(MD5_HASHER_DIR.$this->file_check)));
+		}
+        
+        fclose($fh);
+
+        $rdi = new RecursiveDirectoryIterator($this->dir);
+        $rii = new RecursiveIteratorIterator($rdi);
+        foreach($rii as $name => $obj){
+            $dir_file = $obj->getRealPath(); 
+            $md5_gen_output[$dir_file] = array(
+                'md5' => md5_file($dir_file),
+                'filename' => $obj->getFilename(),
+                'real_path' => $dir_file
+            );
+
+            if(!isset($md5_gen_old[$dir_file]->md5)){
+                // new file
+                $md5_changed_output[$dir_file] = array(
+                    'md5' => md5_file($dir_file),
+                    'filename' => $obj->getFilename(),
+                    'real_path' => $dir_file,
+                    'modified' => 'new'
+                );
+            }else if($md5_gen_old[$dir_file]->md5 !== $md5_gen_output[$dir_file]['md5']){
+                // modified file
+                $md5_changed_output[$dir_file] = array(
+                    'md5' => md5_file($dir_file),
+                    'filename' => $obj->getFilename(),
+                    'real_path' => $dir_file,
+                    'modified' => 'edited'
+                ); 
+            }
+        }
+
+        // save new md5 hashes
+        $fh = fopen(MD5_HASHER_DIR.$this->file_check, 'w');
+        fwrite($fh, json_encode($md5_gen_output));
+        fclose($fh);
+
+        // save changed file_list
+        if(is_file(MD5_HASHER_DIR . $this->file_change)){
+	        $fh = fopen(MD5_HASHER_DIR.$this->file_change, 'a');
+	        fwrite($fh, date('d/m/Y h:i:s').":\n\n");
+	        foreach($md5_changed_output as $k => $v){
+	            fwrite($fh, $v['filename'].' => '.$v['modified']. "\n");
+	        }
+	        fwrite($fh, "\n");
+        }else{
+        	$fh = fopen(MD5_HASHER_DIR.$this->file_change, 'x');
+        }
+        fclose($fh); 
+	    
+	}
+}
+
+$MD5_Hasher = new MD5_Hasher();
